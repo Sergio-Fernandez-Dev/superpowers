a93114e845c04e763063276ae0adb3d542c8078b
Atcc627 <alangarciamartinez627@gmail.com>
2022-06-28 14:03:12 +0200

    create button delete and edit

--- src/views/FavouritesView.vue
+++ src/views/FavouritesView.vue
@@ -1,8 +1,17 @@
-<script></script>
+<script>
+import DeleteButton from "../components/DeleteButton.vue";
+import EditButton from "../components/EditButton.vue";
+export default{
+  components:{DeleteButton, EditButton}
+  
+}
+</script>
 
 <template>
   <div>
     <h1>FAVOURITES</h1>
+    <DeleteButton />
+    <EditButton /> 
   </div>
 </template>
 
