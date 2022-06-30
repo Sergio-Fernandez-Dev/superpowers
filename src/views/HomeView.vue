5d1d93a0f008b52e508457884259a88a866e2b0e
Miguel <miguelsalvador.rguez@gmail.com>
2022-06-30 11:34:41 +0200

    Añadida llamada a la API

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,12 @@
-<script>
-export default {
-
-};
-</script>
+<script></script>
 
 <template>
   <div>
     <h1>HOME</h1>
+    <table>
+      <tr :v-for="(superheroe, index) in superheroes"></tr>
+      <td>{{ superheroe.name }}</td>
+    </table>
   </div>
 </template>
 
