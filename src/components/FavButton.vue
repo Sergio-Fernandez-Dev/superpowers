834d9eb86fc5a4760571dff1e5105396a3ee55df
Sergio-Fernandez-Dev <sr.sergiofernandezfernandez@gmail.com>
2022-07-04 15:32:47 +0200

    testing fav button

--- src/components/FavButton.vue
+++ src/components/FavButton.vue
@@ -1,23 +1,21 @@
 <script>
+import { mapActions } from 'pinia';
 import { favList } from '../store/favList';
-export default { 
-      props: {
-        favHero: Object
-
-      },
-      methods:{
-        ...mapActions(favList,[addHeroToFavs])
-      }
-        
-  };
+export default {
+  props: {
+    favHero: Object,
+  },
+  methods: {
+    ...mapActions(favList, ['addHeroToFavs']),
+  },
+};
 </script>
-  <!--<img src="C:\Superpowers\superpowers\src\components\icons\FavouriteButtonIcon.svg" /> -->
 <template>
   <button @click="addHeroToFavs(favHero)">ADD FAV</button>
 </template>
 
 <style>
-.addfavourite{
+.addfavourite {
   display: flex;
   justify-content: end;
   margin: 0 7px 7px 0;
--- src/store/favList.js
+++ src/store/favList.js
@@ -1,19 +1,15 @@
-import {defineStore} from 'pinia';
+import { defineStore } from 'pinia';
 //Uso de Contador de Storage Local de nuestro favoritos
 export const favList = defineStore({
-    //Section de nuestro propiedades
-    state:()=>{
-        return {list: []}
-        
+  //Section de nuestro propiedades
+  state: () => {
+    return { list: [] };
+  },
+  //Section de Getters que nos devuelve la informacion x2
+  getters: {},
+  actions: {
+    addHeroToFavs(hero) {
+      this.list.push(hero);
     },
-    //Section de Getters que nos devuelve la informacion x2
-    getters: {
-       
-    },
-    actions: {
-       addHeroToFavs(hero){
-         this.list.push(hero);
-       }
-
-    }
+  },
 });
